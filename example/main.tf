7e8e53ac0dd8035c33280e350f097e45bba5ec75
Andre Torres <andreh.torres@hotmail.com>
2023-02-01 20:38:37 -0300

    fix: refactoring

--- example/main.tf
+++ example/main.tf
@@ -1,32 +1,31 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
     }
   }
 }
 
 locals {
-  aws_region = "sa-east-1"
-
+  aws_region  = "us-east-1"
+  queue_name  = "sre-dev"
+  dlq_name    = "sre-dev-dlq"
   common_tags = {
-    team    = "tech-sre"
-    owner   = "tech-tweakers"
-    product = "portal-api"
-    env     = "development"
+    team      = "tech-sre"
+    owner     = "tech-tweakers"
+    product   = "portal-api"
+    env       = "development"
   }
 }
 
 provider "aws" {
-  region  = local.aws_region
+  region      = local.aws_region
 }
 
 module "sqs-queue" {
-  source = "../"
-
-  queue_name = "test-123"
-  dlq_name = "test-123"
-
-  create_dlq                     = true
-  tags                           = local.common_tags
+  source      = "../"
+  tags        = local.common_tags
+  queue_name  = local.queue_name
+  dlq_name    = local.dlq_name
+  create_dlq  = true
 }
